dafe9ce781607df7f4c80fd0198410e1271f68af
alexsbp <alex1546@student.eadania.dk>
2018-04-09 13:47:54 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DbmeterPage } from "../pages/dbmeter/dbmeter";
-<<<<<<< HEAD
 import { GeoFencePage } from "../pages/geo-fence/geo-fence";
-=======
-import { ShakePage } from "../pages/shake/shake";
->>>>>>> 3b73abef97351297afa9becc88779f4726670a35
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     DbmeterPage,
-<<<<<<< HEAD
     GeoFencePage
-=======
-    ShakePage
->>>>>>> 3b73abef97351297afa9becc88779f4726670a35
   ],
   imports: [
     BrowserModule,
@@ -33,11 +25,7 @@ import { ShakePage } from "../pages/shake/shake";
     MyApp,
     HomePage,
     DbmeterPage,
-<<<<<<< HEAD
     GeoFencePage
-=======
-    ShakePage
->>>>>>> 3b73abef97351297afa9becc88779f4726670a35
   ],
   providers: [
     StatusBar,
--- src/pages/dbmeter/dbmeter.ts
+++ src/pages/dbmeter/dbmeter.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-<<<<<<< HEAD
 import { GeoFencePage } from '../geo-fence/geo-fence';
-=======
 import { DBMeter } from '@ionic-native/db-meter';
->>>>>>> 3b73abef97351297afa9becc88779f4726670a35
+
 
 /**
  * Generated class for the DbmeterPage page.
@@ -23,23 +21,23 @@ export class DbmeterPage {
   private subscription:any;
   constructor(public navCtrl: NavController, public navParams: NavParams, private dbMeter: DBMeter) {
   }
-
-<<<<<<< HEAD
   OpenGeoFencePage() {
     this.navCtrl.push(GeoFencePage);
-=======
+  }
+    
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad DbmeterPage');
     this.subscription = this.dbMeter.start().subscribe(
       data => console.log(data)
     );
->>>>>>> 3b73abef97351297afa9becc88779f4726670a35
+
   }
   OpenDBMeterPage() {
     this.navCtrl.push(DbmeterPage);
   }
   OpenFingerPrintPage() {
-    //this.navCtrl.push();
+    
   }
 
 
