025b82118c40442382ccbd604fe841c52bbaf287
Nikolaj Vestergaard Hentze <niko0654@student.eadania.dk>
2018-04-09 13:00:58 +0200

    This is fingerprint / shake page. Dont mind the words or the letters in this one you will have to be a man to read this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DbmeterPage } from "../pages/dbmeter/dbmeter";
+import { ShakePage } from "../pages/shake/shake";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    DbmeterPage
+    DbmeterPage,
+    ShakePage
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,8 @@ import { DbmeterPage } from "../pages/dbmeter/dbmeter";
   entryComponents: [
     MyApp,
     HomePage,
-    DbmeterPage
+    DbmeterPage,
+    ShakePage
   ],
   providers: [
     StatusBar,
