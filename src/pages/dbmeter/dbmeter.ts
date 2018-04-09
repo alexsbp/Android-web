9876042bdb3994c991569d0ba024b113616efae6
S Krogshede <novus@outlook.dk>
2018-04-09 12:04:47 +0200

    s

--- src/pages/dbmeter/dbmeter.ts
+++ src/pages/dbmeter/dbmeter.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DBMeter } from '@ionic-native/db-meter';
 
 /**
  * Generated class for the DbmeterPage page.
@@ -15,11 +16,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DbmeterPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private dbMeter: DbmeterPage) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DbmeterPage');
   }
+
+  // Start listening
+let subscription = this.dbMeter.start().subscribe(
+  data => console.log(data)
+);
+
+// Check if we are listening
+this.dbMeter.isListening().then(
+  isListening => console.log(isListening)
+);
+
+// Stop listening
+subscription.unsubscribe();
+
+// Delete DBMeter instance from memory
+this.dbMeter.delete().then(
+  () => console.log('Deleted DB Meter instance'),
+  error => console.log('Error occurred while deleting DB Meter instance')
 
 }
