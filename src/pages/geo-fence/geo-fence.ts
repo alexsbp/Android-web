086b96aadc7268e6f3ee655b1582abc0a2d50cc7
alexsbp <alex1546@student.eadania.dk>
2018-04-13 10:05:57 +0200

    db meter works

--- src/pages/geo-fence/geo-fence.ts
+++ src/pages/geo-fence/geo-fence.ts
@@ -16,38 +16,45 @@ import { Geofence } from '@ionic-native/geofence';
 })
 export class GeoFencePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private geofence: Geofence) {
+  notification: any; 
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private geofence: Geofence) 
+  {
     // initialize the plugin
     geofence.initialize().then(
     // resolved promise does not return a value
     () => console.log('Geofence Plugin Ready'),
     (err) => console.log(err)
     )
-
+    
     this.addGeofence(); 
   }
+  
 
-  private addGeofence() {
+  public addGeofence() 
+  {
+    
     //options describing geofence
-    let fence = {
+    let fence = 
+    {
       id: 'DaniaGames', //any unique ID
       latitude:       56.410445, //center of geofence radius
       longitude:      10.886268,
-      radius:         5, //radius to edge of geofence in meters
+      radius:         3, //radius to edge of geofence in meters
       transitionType: 3, //see 'Transition Types' below
-      notification: { //notification settings
-          id:             5, //any unique ID
+      notification: 
+      { //notification settings
+          id:             1, //any unique ID
           title:          'You crossed a fence', //notification title
           text:           'You just arrived to Gliwice city center.', //notification body
           openAppOnClick: true //open app when notification is tapped
       }
     }
-  
     this.geofence.addOrUpdate(fence).then(
-       () => console.log('Geofence added'),
-       (err) => console.log('Geofence failed to add')
-     );
+      () => console.log('Geofence added'),
+      (err) => console.log('Geofence failed to add')
+    );
+    
   }
 
 }
